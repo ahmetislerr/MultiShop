e84ea62a6db9ebbd12ebdef04fb43f47e3f21c43
ahmetislerr <ahmetislertr@gmail.com>
2024-07-10 22:44:50 +0300

    Order detail ById,remove,update query handler created.

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/RemoveOrderDetailQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers
+{
+    public class RemoveOrderDetailQueryHandler
+    {
+        private readonly IRepository<OrderDetail> _repository;
+
+        public RemoveOrderDetailQueryHandler(IRepository<OrderDetail> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(RemoveOrderDetailCommand command)
+        {
+            var value = await _repository.GetByIdAsync(command.Id);
+            await _repository.DeleteAsync(value);
+        }
+    }
+}
