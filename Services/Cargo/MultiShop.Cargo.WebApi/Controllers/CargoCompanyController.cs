4fb1df1491961d82632e03ed4ee50b27c18682a7
ahmetislerr <ahmetislertr@gmail.com>
2024-07-28 23:17:28 +0300

    Cargo Company Controller created. Cargo Company Create and Update Dtos created.

---
+++ Services/Cargo/MultiShop.Cargo.WebApi/Controllers/CargoCompanyController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.DtoLayer.Dtos.CargoCompanyDtos;
+using MultiShop.Cargo.EntityLayer.Concrete;
+
+namespace MultiShop.Cargo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CargoCompanyController : ControllerBase
+    {
+        private readonly ICargoCompanySevice _cargoCompanySevice;
+
+        public CargoCompanyController(ICargoCompanySevice cargoCompanySevice)
+        {
+            _cargoCompanySevice = cargoCompanySevice;
+        }
+
+        [HttpGet]
+        public IActionResult CargoCompanyList()
+        {
+            var values = _cargoCompanySevice.TGetAll();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCargoCompany(CreateCargoCompanyDto createCargoCompanyDto)
+        {
+            CargoCompany cargoCompany = new CargoCompany()
+            {
+                CargoCompanyName = createCargoCompanyDto.CargoCompanyName,
+            };
+            _cargoCompanySevice.TInsert(cargoCompany);
+            return Ok("Kargo Şirketi Başarıyla Oluşturuldu");
+        }
+
+        [HttpDelete]
+        public IActionResult RemoveCargoCompany(int id)
+        {
+            _cargoCompanySevice.TDelete(id);
+            return Ok("Kargo Şirketi Başarıyla Silindi");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCargoCompanyById(int id)
+        {
+            var values = _cargoCompanySevice.TGetById(id);
+            return Ok(values);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCargoCompany(UpdateCargoCompanyDto updateCargoCompanyDto)
+        {
+            CargoCompany cargoCompany = new CargoCompany()
+            {
+                CargoCompanyId = updateCargoCompanyDto.CargoCompanyId,
+                CargoCompanyName = updateCargoCompanyDto.CargoCompanyName
+            };
+            _cargoCompanySevice.TUpdate(cargoCompany);
+            return Ok("Kargo Şirketi Başarıyla Güncellendi");
+        }
+    }
+}
