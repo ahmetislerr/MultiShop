913ca380fa6fe1ee83b67d22c3ad0e77c4c22b6c
ahmetislerr <ahmetislertr@gmail.com>
2024-07-31 16:55:20 +0300

    Basket Services, Login Services created.

---
+++ Services/Basket/MultiShop.Basket/Services/BasketServices/IBasketService.cs
@@ -0,0 +1,11 @@
+﻿using MultiShop.Basket.Dtos;
+
+namespace MultiShop.Basket.Services.BasketServices
+{
+    public interface IBasketService
+    {
+        Task<BasketTotalDto> GetBasket(string userId);
+        Task SaveBasket(BasketTotalDto basketTotalDto);
+        Task DeleteBasket(string userId);
+    }
+}
