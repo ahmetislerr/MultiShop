88c27be1bb3b62a180fd2ec15ac7844b824b1a94
ahmetislerr <ahmetislertr@gmail.com>
2024-08-17 23:43:35 +0300

    Category Create Dto ,Action Method  and View created. Category Delete Action Method created.

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -2,10 +2,13 @@
 using Microsoft.AspNetCore.Mvc;
 using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
 	[Area("Admin")]
+	[AllowAnonymous]
+	[Route("Admin/Category")]
 	public class CategoryController : Controller
 	{
 		private readonly IHttpClientFactory _httpClientFactory;
@@ -15,7 +18,7 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
 			_httpClientFactory = httpClientFactory;
 		}
 
-		[AllowAnonymous]
+		[Route("Index")]
 		public async Task<IActionResult> Index()
 		{
 			ViewBag.v0 = "Kategori İşlemleri";
@@ -32,6 +35,45 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
 				return View(values);
 			}
 
+			return View();
+		}
+
+		[Route("CreateCategory")]
+		[HttpGet]
+		public IActionResult CreateCategory()
+		{
+			ViewBag.v0 = "Kategori İşlemleri";
+			ViewBag.v1 = "Ana Sayfa";
+			ViewBag.v2 = "Kategoriler";
+			ViewBag.v3 = "Yeni Kategori Girişi";
+			return View();
+		}
+
+		[Route("CreateCategory")]
+		[HttpPost]
+		public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(createCategoryDto);
+			StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+			var responseMessage = await client.PostAsync("http://localhost:7070/api/Categories", stringContent);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index","Category", new {area = "Admin"});
+			}
+			return View();
+		}
+
+
+		[Route("DeleteCategory/{id}")]
+		public async Task<IActionResult> DeleteCategory(string id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.DeleteAsync("http://localhost:7070/api/Categories?id=" + id);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index", "Category",new {area = "Admin"});
+			}
 			return View();
 		}
 	}
