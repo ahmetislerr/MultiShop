b0d05e177bd07024d5881c6d0da7faf6c71e7dd2
ahmetislerr <ahmetislertr@gmail.com>
2024-08-19 22:56:46 +0300

    Category Update Dto and Action Method created.

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -76,5 +76,39 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
 			}
 			return View();
 		}
+
+		[Route("UpdateCategory/{id}")]
+		[HttpGet]
+		public async Task<IActionResult> UpdateCategory(string id)
+		{
+			ViewBag.v0 = "Kategori İşlemleri";
+			ViewBag.v1 = "Ana Sayfa";
+			ViewBag.v2 = "Kategoriler";
+			ViewBag.v3 = "Kategori Güncelleme Sayfası";
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("http://localhost:7070/api/Categories/" + id);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<UpdateCategoryDto> (jsonData);
+				return View(values);
+			}
+			return View();
+		}
+
+		[Route("UpdateCategory/{id}")]
+		[HttpPost]
+		public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(updateCategoryDto);
+			StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+			var responseMessage = await client.PutAsync("http://localhost:7070/api/Categories/", stringContent);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index", "Category", new { area = "Admin" });
+			}
+			return View();
+		}
 	}
 }
